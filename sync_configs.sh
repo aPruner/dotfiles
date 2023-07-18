c5c1282660f6e95bf0a63e9d791680530e466b96
aPruner <adampruner99@gmail.com>
2023-07-18 14:48:32 -0700

    forgot to add git pull to the script

--- sync_configs.sh
+++ sync_configs.sh
@@ -1,5 +1,6 @@
 # Small lil script to copy the updated config.lua file
 # into the .config directory and launch neovim to sync packages
 # as the config grows more complex, this script will have to evolve
+git pull
 cp config.lua ~/.config/lvim/config.lua
 lvim
