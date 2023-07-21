3f3e979035f9550ecd1beb134941ec202129b87c
aPruner <adampruner99@gmail.com>
2023-07-21 15:48:18 -0700

    Fix tsserver config to disable it for JS files for now, tried to get flow working but failed

--- sync_configs.sh
+++ sync_configs.sh
@@ -3,4 +3,7 @@
 # as the config grows more complex, this script will have to evolve
 git pull
 cp config.lua ~/.config/lvim/config.lua
+# TODO: As the config grows and eventually I start splitting things out into more
+# separate folders/files, I should create a list and loop through it in this script
+cp -R ftplugin ~/.config/lvim
 lvim
