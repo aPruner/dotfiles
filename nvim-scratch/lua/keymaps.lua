b77486862b9ea1eb08d94fa741ec5f58d4ee6623
aPruner <adampruner99@gmail.com>
2023-07-23 16:10:25 -0700

    progress on custom config

---
+++ nvim-scratch/lua/keymaps.lua
@@ -0,0 +1,30 @@
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- NvimTree keymaps
+vim.keymap.set('n', '<leader>e', "<cmd>:NvimTreeToggle<CR>", {})
+
+-- Map buffer switching
+vim.keymap.set('n', '<Tab>', "<cmd>:bprev<CR>", {})
+vim.keymap.set('n', '<S-Tab>', "<cmd>:bnext<CR>", {})
+
+-- Window split switching
+vim.keymap.set('n', '<C-h>', "<cmd>:wincmd h<CR>", {silent = true})
+vim.keymap.set('n', '<C-j>', "<cmd>:wincmd j<CR>", {silent = true})
+vim.keymap.set('n', '<C-k>', "<cmd>:wincmd k<CR>", {silent = true})
+vim.keymap.set('n', '<C-l>', "<cmd>:wincmd l<CR>", {silent = true})
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+
