9fe77415547182641f005afec5b6a01759f50968
aPruner <adampruner99@gmail.com>
2023-07-24 10:22:15 -0700

    add some config for navic but it doesn't work quite yet

--- nvim-scratch/nvim/init.lua
+++ nvim-scratch/nvim/init.lua
@@ -134,7 +134,12 @@ require('lazy').setup({
   {
     "SmiteshP/nvim-navic",
     config = function()
-      require("nvim-navic").setup()
+      require("nvim-navic").setup {
+        lsp = {
+          auto_attach = true,
+          preference = nil,
+        }
+      }
     end,
   },
 
